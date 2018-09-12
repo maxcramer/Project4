736cda1f5ff929e130ec490d5093b821f56ef672
Max Cramer <maximiliancramer@yahoo.com>
2018-09-12 11:26:54 +0100

    ready for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/project4';
 const secret = process.env.SECRET || 'onewheelisgod';
+const port = process.env.PORT || 4000;
 
 module.exports = {
-  dbUri, secret
+  dbUri, secret, port
 };
